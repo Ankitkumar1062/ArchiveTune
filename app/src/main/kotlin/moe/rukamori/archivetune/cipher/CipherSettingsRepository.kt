2763de855281c504ba0efc1ce1b723980ad5bcd9
vossgraves <129050389+vossgraves@users.noreply.github.com>
2026-08-03 13:55:45 +0530

    mhsm: import MoriCipherRuntime.refresh extension

--- app/src/main/kotlin/moe/rukamori/archivetune/cipher/CipherSettingsRepository.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/cipher/CipherSettingsRepository.kt
@@ -17,6 +17,7 @@ import moe.rukamori.archivetune.constants.MoriCipherManualRefreshHistoryKey
 import moe.rukamori.archivetune.morideobfuscator.CipherRefreshResult
 import moe.rukamori.archivetune.morideobfuscator.CipherSnapshot
 import moe.rukamori.archivetune.morideobfuscator.MoriCipherRuntime
+import moe.rukamori.archivetune.morideobfuscator.refresh
 import moe.rukamori.archivetune.utils.dataStore
 import javax.inject.Inject
 import javax.inject.Singleton
