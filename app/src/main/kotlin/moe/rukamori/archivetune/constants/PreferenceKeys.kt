2d940a66704dabf2f51667877530d93524f6469e
vossgraves <129050389+vossgraves@users.noreply.github.com>
2026-08-03 11:51:16 +0530

    mhsm: remove gatekeeper + MoriCipher + BotGuard + CANVAS_BEARER + token plumbing (1/3)

--- app/src/main/kotlin/moe/rukamori/archivetune/constants/PreferenceKeys.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/constants/PreferenceKeys.kt
@@ -309,11 +309,9 @@ val PlayerStreamClientKey = stringPreferencesKey("playerStreamClient")
 enum class PlayerStreamClient {
     ANDROID_VR,
     WEB_REMIX,
-    ARCHIVETUNE_EXTRACTOR,
-    HI_RES_LOSSLESS,
     IOS,
     TVHTML5,
-    ANDROID_MUSIC,
+    VISIONOS,
 }
 
 val PersistentQueueKey = booleanPreferencesKey("persistentQueue")
