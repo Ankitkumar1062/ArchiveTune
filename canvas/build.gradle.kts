798628c41c444954d93e486e821aca5eb2ea3ca2
Ankitkumar1062 <ankit_k@cs.iitr.ac.in>
2026-09-13 01:52:58 +0530

    fix(build): align protobuf dependency to 3.11.0 to eliminate duplicate classes

--- canvas/build.gradle.kts
+++ canvas/build.gradle.kts
@@ -8,7 +8,7 @@ kotlin {
 }
 
 dependencies {
-    implementation(libs.protobuf.javalite)
+    implementation(libs.protobuf.java)
     implementation(libs.ktor.client.core)
     implementation(libs.ktor.client.okhttp)
     implementation(libs.ktor.client.content.negotiation)
